e63f1d8508cc1dff10b8c404d0e11215b8941d6e
Martin Yoakum <myoakum@wustl.edu>
2018-01-29 22:33:04 -0600

    main nav style

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'yom' ); ?></button>
 			<?php
 			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
+				'theme_location' => 'primary',
 				'menu_id'        => 'primary-menu',
 			) );
 			?>
