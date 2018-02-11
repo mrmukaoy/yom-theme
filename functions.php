99997a40aad6f4064888cb10ff1ce25c8e23514d
Martin Yoakum <myoakum@wustl.edu>
2018-02-10 21:24:35 -0600

    show excerpts on archive pages

--- functions.php
+++ functions.php
@@ -78,17 +78,6 @@ if ( ! function_exists( 'yom_setup' ) ) {
 add_action( 'after_setup_theme', 'yom_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function yom_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'yom_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'yom_content_width', 0 );
-
-/**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
